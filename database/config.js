bd1f1f08e8658b6b0ec8b9e9a4009c3a09cb119e
Fernando Casique <feerka.eng@gmail.com>
2023-03-29 22:51:03 +0200

    Se realiza CRUD de medicos y hospitales. Se realizan busquedas y login con JWT

--- database/config.js
+++ database/config.js
@@ -4,23 +4,14 @@ const mongoose = require('mongoose');
 const dbConnection = async() => {
 
     try {
-<<<<<<< HEAD
         await mongoose.connect( process.env.DB_CNN , {
             useNewUrlParser: true, 
-=======
-        await mongoose.connect(process.env.DB_CNN, {
-            useNewUrlParser: true,
->>>>>>> 34479fa30d37f93b981dc087c1668d310b928c9a
             useUnifiedTopology: true,
             useCreateIndex: true
         });
 
         console.log('DB Online');
-<<<<<<< HEAD
         
-=======
-
->>>>>>> 34479fa30d37f93b981dc087c1668d310b928c9a
     } catch (error) {
         console.log(error);
         throw new Error('Error a la hora de iniciar la BD ver logs');
